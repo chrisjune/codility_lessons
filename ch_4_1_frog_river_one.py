0075cbe20269543235b06bf8bf86ccac6a4353e6
chrisjune <chrisjune@gmail.com>
2020-07-30 10:53:10 +0900

    add ch 4_2 max counter with O(N+M)

--- ch_4_1_frog_river_one.py
+++ ch_4_1_frog_river_one.py
@@ -1,17 +1,21 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(X, A):
-    # write your code in Python 3.6
-    counter = [0] * (max(A)+1)
-    for i in range(len(A)):
-        counter[A[i]] += 1
-    for i in range(len(A)):
-        return -1    
 
-# assert solution(5, [1,1,1,1,1,1,1,1]) == -1
-# assert solution(5, [1,3,1,4,2,3,5,4]) == 6
-assert solution(1, [1]) == 0
+def solution(X, A):
+    check = [False] * X
+    cnt = 0
+    for i in range(len(A)):
+        if check[A[i] - 1] == False:
+            check[A[i] - 1] = True
+            cnt += 1
+        if cnt == X:
+            return i
+    return -1
+
+
 assert solution(2, [1, 2]) == 1
 assert solution(3, [1, 2, 3]) == 2
 assert solution(3, [1, 3, 2]) == 2
+assert solution(5, [1,1,1,1,1,1,1,1]) == -1
+assert solution(5, [1,3,1,4,2,3,5,4]) == 6
