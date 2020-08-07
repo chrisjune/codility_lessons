82c544107089a247321027801dbb98ca9fac91c7
chrisjune <chrisjune@gmail.com>
2020-08-07 23:47:59 +0900

    Add longest_valid_parentheses

--- longest_valid_parentheses.py
+++ longest_valid_parentheses.py
@@ -1,8 +1,19 @@
 class Solution:
     def longestValidParentheses(self, s):
-        pass
+        stack = []
+        dp = [0] * len(s)
+        for idx, ele in enumerate(s):
+            if ele == '(':
+                stack.append(idx)
+            else:
+                if stack:
+                    left_position = stack.pop()
+                    dp[idx] = dp[left_position-1] + idx - left_position + 1
+        return max(dp)
+
 
 assert Solution().longestValidParentheses('(()') == 2
+assert Solution().longestValidParentheses('()()') == 4
 assert Solution().longestValidParentheses(')()())') == 4
 
 
